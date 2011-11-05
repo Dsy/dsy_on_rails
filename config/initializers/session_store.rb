47201ba0d8eabc14002e7fb0d1b68fb85233bb5e
Walter Mottinelli <wmotti@gmail.com>
2011-11-05 17:12:53 +0100

    Initial import and development db setup

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DsyOnRails::Application.config.session_store :cookie_store, key: '_dsy_on_rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DsyOnRails::Application.config.session_store :active_record_store
