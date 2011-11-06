2fdd20679694d91e9ca294704801fb6d324ed1e1
Walter Mottinelli <wmotti@gmail.com>
2011-11-06 15:24:34 +0100

    Added configuration for unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,15 @@
+# Minimal sample configuration file for Unicorn (not Rack) when used
+# with daemonization (unicorn -D) started in your working directory.
+#
+# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
+# documentation.
+# See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
+# a more verbose configuration using more features.
+#
+app_path = "/home/wmotti/dev/dsy_on_rails"
+
+listen 2007 # by default Unicorn listens on port 8080
+worker_processes 2 # this should be >= nr_cpus
+pid "#{app_path}/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/log/unicorn.log"
+stdout_path "#{app_path}/log/unicorn.log"
