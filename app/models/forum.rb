bf3bcfa48eea1847d10ead6fc1e1c35f7cf004c0
Walter Mottinelli <wmotti@gmail.com>
2012-02-08 23:29:35 +0100

    Forum model/controller/views updated!

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,5 +1,5 @@
 class Forum < ActiveRecord::Base
-  has_many :topics
+  has_many :topics, after_add: :increment_topics_count, after_remove: :decrement_topics_count
   has_many :posts, through: :topics
   belongs_to :last_post, class_name: 'Post', foreign_key: :last_post_id
   has_many :children_forums, class_name: 'Forum', foreign_key: :parent_id
@@ -46,5 +46,29 @@ class Forum < ActiveRecord::Base
 
   def last_poster
     last_post.user if last_post
+  end
+
+  def increment_topics_count(topic)
+    forum = Forum.find (topic.forum_id)
+    #Rails.logger.debug self.to_yaml if topics_count.nil?
+    #Rails.logger.debug '>>> topics count => ' + topics_count.to_s
+    #Rails.logger.debug '>>> inspect topics_count => ' + topics_count.inspect
+    #Rails.logger.debug '>>> class topics_count => ' + topics_count.class.to_s
+    #Forum.increment_counter(:topics_count, forum.id)
+    forum.increment! :topics_count
+    forum.parent_chain.each do |f|
+      #Forum.increment_counter(:topics_count, f.id)
+      f.increment! :topics_count
+    end
+  end
+
+  def decrement_topics_count(topic)
+    forum = Forum.find (topic.forum_id)
+    #Forum.decrement_counter(:topics_count, forum.id)
+    f.decrement! :topics_count
+    forum.parent_chain.each do |f|
+      #Forum.decrement_counter(:topics_count, f.id)
+      f.decrement! :topics_count
+    end
   end
 end
