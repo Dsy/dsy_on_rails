5621941966d5ea4b1ed40f9001e3457ec9e5ab74
Walter Mottinelli <wmotti@gmail.com>
2012-02-08 23:29:00 +0100

    Topic model/controller/views updated!

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -8,7 +8,56 @@ class TopicsController < ApplicationController
       redirect_to :action => :index, :alert => 'The topic you specified was not found!'
     end
     @posts = @topic.posts.by_dateline
+    @topic.increment! :views
 
     respond_with @posts
+  end
+
+  def new
+    @topic = Topic.new forum_id: params[:forum_id]
+    @post = Post.new
+  end
+
+  def create
+    @topic = Topic.new params[:topic]
+    @topic.title = params[:post][:title]
+    forum = Forum.find params[:forum_id]
+    respond_to do |format|
+      if @topic.save
+        forum.topics << @topic
+        #logger.debug forum.to_yaml
+        #forum.topics_count += 1
+        #forum.save
+        #forum.increase_topics_count
+        #params[:post][:topic_id] = @topic.id
+        @post  = Post.new params[:post]
+        @topic.posts << @post
+        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
+      else
+        format.html { render action: :new }
+      end
+    end
+  end
+
+  def edit
+    @topic = Topic.find params[:id]
+  end
+
+  def update
+    @topic = Topic.find params[:id]
+    respond_to do |format|
+      if @topic.update_attributes params[:topic]
+        format.html  { redirect_to @topic, notice: 'Topic was successfully updated.' }
+      else
+        format.html  { render :action => "edit" }
+      end
+    end
+  end
+
+  def destroy
+    @topic = Topic.find params[:id]
+    @topic.destroy
+    @topic.forum.decrease_topics_count
+    redirect_to topics_path
   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,7 +3,10 @@ class Topic < ActiveRecord::Base
   belongs_to :forum
   belongs_to :last_post, class_name: 'Post', foreign_key: :last_post_id
   belongs_to :starter, class_name: 'User', foreign_key: :starter_id
-  has_many :posts, dependent: :destroy
+  has_many :posts,
+    after_add: [ :increment_posts_count, :update_last_post_id ],
+    after_remove: :decrement_posts_count,
+    dependent: :destroy
 
   def self.by_dateline
     #FIXME
@@ -18,9 +21,9 @@ class Topic < ActiveRecord::Base
     last_post.user if last_post
   end
 
-  def created_at
-    first_post.created_at if first_post
-  end
+  #def created_at
+    #first_post.created_at if first_post
+  #end
 
   def last_post_date
     last_post.to_date.to_s :long
@@ -29,5 +32,58 @@ class Topic < ActiveRecord::Base
   def last_post_time
     last_post.to_s :time
   end
+
+  private
+
+  def increment_posts_count(post)
+    topic = Topic.find post.topic_id
+    #Topic.increment_counter(:posts_count, topic.id)
+    topic.increment! :posts_count
+    forum = topic.forum
+    #Forum.increment_counter(:posts_count, forum.id)
+    forum.increment! :posts_count
+    forum.parent_chain.each do |f|
+      #Forum.increment_counter(:posts_count, f.id)
+      f.increment! :posts_count
+    end
+    #User.increment_counter(:posts_count, post.user_id)
+    #user = post.user
+    #user.increment! :posts_count
+  end
+
+  def update_last_post_id(post)
+    topic = Topic.find post.topic_id
+    #topic.last_post_id = post.id
+    #topic.save
+    topic.update_attributes(last_post_id: post.id)
+    forum = topic.forum
+    #forum.last_post_id = post.id
+    #forum.save
+    forum.update_attributes(last_post_id: post.id)
+    forum.parent_chain.each do |f|
+      #f.last_post_id = post.id
+      #f.save
+      f.update_attributes(last_post_id: post.id)
+    end
+    user = post.user
+    #user.last_post_id = post.id
+    #user.save
+    #user.update_attributes(last_post_id: post.id)
+  end
+
+  def decrement_posts_count(post)
+    topic = Topic.find post.topic_id
+    #Topic.decrement_counter(:posts_count, topic.id)
+    topic.decrement! :posts_count
+    forum = topic.forum
+    #Forum.decrement_counter(:posts_count, forum.id)
+    forum.decrement! :posts_count
+    forum.parent_chain.each do |f|
+      #Forum.decrement_counter(:posts_count, f.id)
+      forum.decrement! :posts_count
+    end
+    user = post.user
+    user.decrement! :posts_count
+ end
 
 end
