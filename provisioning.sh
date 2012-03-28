f9ddcd4fe3d672336cc4e1057d9d8c0df41c6628
Walter Mottinelli <wmotti@gmail.com>
2012-03-29 00:28:54 +0200

    Modified provisioning for starting unicorn

--- provisioning.sh
+++ provisioning.sh
@@ -8,14 +8,15 @@ if [[ -e Gemfile ]]; then
     bundle install
 fi
 
-if [[ ! -e  config/unicorn-vagrant.rb ]]; then
-    cp config/unicorn.rb.dist config/unicorn-vagrant.rb
-fi
-
 if [[ ! -e  config/database.yml ]]; then
     cp config/database.yml.dist config/database.yml
     echo "Importing sample data in the development db..."
     mysql -udsy -pdsy dsy_development < db/dump_dsy_development.sql
     echo "Done."
-    echo "Visit http://localhost:3000 and have fun!"
 fi
+
+if [[ ! -e  config/unicorn-vagrant.rb ]]; then
+    cp config/unicorn.rb.dist config/unicorn-vagrant.rb
+fi
+/etc/rc.d/unicorn start
+echo "Visit http://localhost:3000 and have fun!"
